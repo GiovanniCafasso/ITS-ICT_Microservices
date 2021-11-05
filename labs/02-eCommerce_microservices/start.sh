2140f0eb8af23a0bb4045973b3d5e229e461665e
GiovanniCafasso <giovanni.cafasso01@gmail.com>
2021-11-05 17:21:06 +0100

    Commented flag '--build' of 02-lab

--- labs/02-eCommerce_microservices/start.sh
+++ labs/02-eCommerce_microservices/start.sh
@@ -1,4 +1,4 @@
 export DOCKER_CLIENT_TIMEOUT=120
 export COMPOSE_HTTP_TIMEOUT=120
 
-docker-compose up --build
+docker-compose up #--build   # con questo flag rebuilda ogni volta da zero che se l'immagine è presente in locale
